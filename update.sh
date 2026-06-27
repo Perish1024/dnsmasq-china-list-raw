4c72a20dd0037074b7f58b9b09f666caf2c3095d
Perish1024 <perish1024@proton.me>
2026-06-27 19:07:46 +0000

    init

---
+++ update.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# update submodule
+git submodule update --init --remote
+# commit
+if [[ $(git diff --stat) != '' ]]; then
+  git config --local user.email "github-actions[bot]@users.noreply.github.com"
+  git config --local user.name "github-actions[bot]"
+  pushd ./dnsmasq-china-list
+  make raw
+  popd
+  cp -v ./dnsmasq-china-list/*.raw.txt .
+  git add .
+  git commit -m "$(TZ=Asia/Shanghai date +%Y-%m-%dT%H:%M:%S%z) update"
+  git push
+fi
